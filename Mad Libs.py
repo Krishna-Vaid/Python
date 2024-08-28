ef15433b7ae4034c003a4fa9a0aee092fd0a4183
Krishna-Vaid <krishnavaid05@gmail.com>
2024-08-28 23:42:37 +0530

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,15 @@
+# Taking inputs from user 
+noun = input('Choose a noun: ')
+p_noun = input('Choose a plural noun: ')
+noun2 = input('Choose another noun: ')
+place = input('Name a place: ')
+adjective = input('Choose an adjective (Describing word): ')
+noun3 = input('Choose one more noun: ')
+
+# Printing then story from the user input
+print('------------------------------------------')
+print('Once upon a time in a', place + ',')
+print('there was a', adjective, noun, 'that loved to play with', p_noun + '.')
+print('One day, it found a', noun2, 'and decided to show it to the other', p_noun + '.')
+print('Everyone was so excited and gathered around the', noun2 + '.')
+print('They all had a great time and celebrated with some', noun3 + '.')
